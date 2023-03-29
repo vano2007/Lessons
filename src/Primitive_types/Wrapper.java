959fb2801194c396aae6fbd84b2c9414641fe3d3
Eduard Angelskiy <ed_angel74@mail.ru>
2023-03-29 19:57:10 +0700

    Commit_290323

---
+++ src/Primitive_types/Wrapper.java
@@ -0,0 +1,15 @@
+package Primitive_types;
+
+public class Wrapper {
+    public static void main(String[] args) {
+        int x=1;
+        System.out.println(x);
+        //Integer x2 = new Integer(123); Java сама упаковывает в класс, следующей записью
+        Integer x3 = 123;
+        System.out.println(x3);
+        int y = x3; // это мы класс распаковали и теперь он является обычным примитивом
+
+        //Классы Double, Float, Long, Integer, Short, Byte, Character, Boolen и у каждого из них есть соответствующий примитивный тип данных
+
+    }
+}
