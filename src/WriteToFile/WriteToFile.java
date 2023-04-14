ea2a1e4d8769b80934cca7ad7922bcbdcb15718c
Eduard Angelskiy <ed_angel74@mail.ru>
2023-04-14 14:44:03 +0700

    Commit_140423

---
+++ src/WriteToFile/WriteToFile.java
@@ -0,0 +1,17 @@
+package WriteToFile;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+public class WriteToFile {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("test_file");
+        PrintWriter pw = new PrintWriter(file);
+
+        pw.println("Test row 1");
+        pw.println("Test row 2");
+
+        pw.close();
+    }
+}
